862d6c673a1e5fbe4512856c5f53addaa178a537
khan-asra <khanasr@sheridancollege.ca>
2023-12-04 00:52:16 -0500

    fractals  commit

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function () {
     const canvas = this.document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = this.window.innerWidth * 0.8;
-    canvas.height = this.window.innerHeight * 0.8;
+    canvas.width = this.window.innerWidth ;
+    canvas.height = this.window.innerHeight ;
     //canvas settings
     ctx.fillStyle = 'green'
     ctx.strokeStyle = 'yellow'
@@ -12,52 +12,64 @@ window.addEventListener('load', function () {
     //ctx.fillRect(50,50,100,100);
     //effect setting
     let size = 200;
+    let sides = 100
+    let maxLevel = 3
+    let spread = 0.8
+    let branches = 2
+    let scale = 0.5
     ctx.save()
     ctx.translate(canvas.width / 2, canvas.height / 2)
     ctx.scale(1, 1)
     ctx.rotate(0)
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-    ctx.beginPath();
-    // ctx.moveTo(canvas.width / 2, canvas.height / 2)
-    ctx.moveTo(0, 0)
-    ctx.lineTo(size, 0)
 
 
-    let sides = 100
-
-    for (let index = 0; index < sides; index++) {
-
+    function drawBranch(level) {
+        if (level > maxLevel) return;
         ctx.beginPath();
         ctx.moveTo(0, 0)
         ctx.lineTo(size, 0)
         ctx.stroke()
-        ctx.rotate((Math.PI*2)/sides)
-        ctx.scale(0.97, 0.97)
+
+        for (let index = 0; index < branches; index++) {
+            ctx.save()
+            ctx.translate(size -(size / branches) * index, 0)
+            ctx.rotate(spread)
+            ctx.scale(scale, scale)
+            drawBranch(level + 1)
+            ctx.restore()
+
+
+            ctx.save()
+            ctx.translate(size-(size / branches) * index, 0)
+            ctx.rotate(-spread)
+            ctx.scale(scale, scale)
+            drawBranch(level + 1)
+            ctx.restore()
+        }
+
 
     }
 
+    drawBranch(0)
+    //ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    // ctx.stroke()
-    // ctx.rotate(0.6)
     // ctx.beginPath();
-    // ctx.moveTo(0,0)
-    // ctx.lineTo(size,0)
+    // ctx.moveTo(canvas.width / 2, canvas.height / 2)
+    // ctx.moveTo(0, 0)
+    // ctx.lineTo(size, 0)
 
 
-    // ctx.stroke()
-    // ctx.rotate(0.6)
-    // ctx.beginPath();
-    // ctx.moveTo(0,0)
-    // ctx.lineTo(size,0)
+    // for (let index = 0; index < sides; index++) {
 
+    //     ctx.beginPath();
+    //     ctx.moveTo(0, 0)
+    //     ctx.lineTo(size, 0)
+    //     ctx.stroke()
+    //     ctx.rotate((Math.PI * 2) / sides)
+    //     ctx.scale(0.97, 0.97)
+    //     ctx.translate(3, 6)
 
-    // ctx.stroke()
-    // ctx.rotate(0.6)
-    // ctx.beginPath();
-    // ctx.moveTo(0,0)
-    // ctx.lineTo(size,0)
-
+    // }
     ctx.restore()
 
 
